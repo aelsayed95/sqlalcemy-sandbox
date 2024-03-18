683fe08441564cf589485706ab92b2d4bb3bd449
Rhythm Patel <42092887+rhythm-patel@users.noreply.github.com>
2024-03-18 15:51:47 +0000

    add total cost of order

--- marketsvc/db_accessor.py
+++ marketsvc/db_accessor.py
@@ -21,11 +21,13 @@ def execute_query(query):
 
     return rows
 
+
 def get_customers():
     rows = execute_query("SELECT * FROM customer")
     for row in rows:
         print(row)
     return rows
+
 
 def get_orders_of_customer(customer_id):
     rows = execute_query(f"""
@@ -47,6 +49,27 @@ def get_orders_of_customer(customer_id):
     for row in rows:
         print(row)
     return rows
+
+
+def get_total_cost_of_an_order(order_id):
+    rows = execute_query(f"""
+        SELECT 
+            SUM(item.price*order_items.quantity)
+        FROM orders 
+        JOIN order_items 
+        ON 
+            order_items.order_id = orders.id 
+        JOIN item 
+        ON 
+            item.id = order_items.item_id
+        WHERE
+            orders.id={order_id}
+        """)
+
+    for row in rows:
+        print(row)
+
+    return {"Order Total": rows[0][0]}
 
 if __name__ == "__main__":
     # get_customers()
--- marketsvc/server.py
+++ marketsvc/server.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 
-from db_accessor import get_customers, get_orders_of_customer
+from db_accessor import get_customers, get_orders_of_customer, get_total_cost_of_an_order
 
 app = Flask(__name__)
 
@@ -17,6 +17,11 @@ def customers():
 def orders():
     cust_id = request.args.get('cust_id')
     return jsonify(get_orders_of_customer(cust_id))
+
+@app.route('/api/order_total')
+def order_total():
+    order_id = request.args.get('order_id')
+    return jsonify(get_total_cost_of_an_order(order_id))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
