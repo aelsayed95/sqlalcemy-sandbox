4d395bc503ac11a953c831fa5a556c43e31771cf
Rhythm Patel <42092887+rhythm-patel@users.noreply.github.com>
2024-03-23 13:12:33 +0000

    add sqlalchemy engine

---
+++ marketsvc/db/base.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine, URL
+
+DB_USER = os.environ.get("POSTGRES_USER")
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+DB_PORT = os.environ.get("POSTGRES_PORT")
+DB_NAME = os.environ.get("POSTGRES_DB")
+DB_HOST = "marketdb"
+
+url_object = URL.create(
+    "postgresql+psycopg2",
+    username=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    database=DB_NAME,
+    port=DB_PORT
+)
+
+engine = create_engine(url_object, echo=True)
