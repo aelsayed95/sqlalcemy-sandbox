2cccea241a5e770b7d91b5da0f9fc5c8c22ed00b
Rhythm Patel <42092887+rhythm-patel@users.noreply.github.com>
2024-03-28 12:32:17 +0000

    Update Item ORM and refactor to new file

---
+++ marketsvc/db/item.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from marketsvc.db.base import Base, engine
+
+
+class Item(Base):
+    __tablename__ = "item"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    price: Mapped[int]
+    description: Mapped[str | None]
+
+    def __repr__(self) -> str:
+        return f"Item(id={self.id!r}, name={self.name!r}, price={self.price!r}), description={self.description!r})"
