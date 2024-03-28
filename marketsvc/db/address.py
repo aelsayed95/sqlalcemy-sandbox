105c55a3a64f5a22c457fe332a8735589f9283bb
Rhythm Patel <42092887+rhythm-patel@users.noreply.github.com>
2024-03-28 12:34:05 +0000

    add ORM for Address

---
+++ marketsvc/db/address.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from marketsvc.db.base import Base, engine
+
+
+class Address(Base):
+    __tablename__ = "address"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    flat_number: Mapped[str]
+    post_code: Mapped[str]
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, flat_number={self.flat_number!r}, post_code={self.post_code!r}))"
