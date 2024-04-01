f97d1a001134ca9cb8312f58721a41337d193cef
Rhythm Patel <42092887+rhythm-patel@users.noreply.github.com>
2024-04-01 09:40:20 +0000

    address int

--- marketsvc/db/address.py
+++ marketsvc/db/address.py
@@ -6,8 +6,8 @@ class Address(Base):
     __tablename__ = "address"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    flat_number: Mapped[str]
-    post_code: Mapped[str]
+    flat_number: Mapped[int]
+    post_code: Mapped[int]
 
     def __repr__(self) -> str:
         return f"Address(id={self.id!r}, flat_number={self.flat_number!r}, post_code={self.post_code!r})"
