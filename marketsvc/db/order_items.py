da14d024a9677919b046cec54ebb363dc0970a7b
Rhythm Patel <42092887+rhythm-patel@users.noreply.github.com>
2024-04-07 20:54:45 +0000

    hybrid property

--- marketsvc/db/order_items.py
+++ marketsvc/db/order_items.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from db.base import Base
 from db.item import Item
 from sqlalchemy import ForeignKey
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
@@ -14,6 +15,10 @@ class OrderItems(Base):
     quantity: Mapped[int]
 
     item: Mapped["Item"] = relationship(lazy="joined")  # many to one
+
+    @hybrid_property
+    def order_total(self) -> float:
+        return self.item.price * self.quantity
 
     def __repr__(self) -> str:
         return f"OrderItems(order_id={self.order_id!r}, item_id={self.item_id!r}, quantity={self.quantity!r}, item={self.item})"
