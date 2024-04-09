25bdefe75ff7081bd3b7ac65a9e00922a494c9ed
Aya Elsayed <aelsayed12@bloomberg.net>
2024-04-09 18:22:33 +0000

    diffing

--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
-docker compose run -p 9090:9090 marketsvc
-# curl -v http://localhost:9090/
-# curl http://localhost:9090/api/customers
-# curl http://localhost:9090/api/orders?cust_id=1
-# curl http://localhost:9090/api/order_total?order_id=1
-# curl "http://localhost:9090/api/orders_between_dates?after=2024-03-14&before=2024-03-22"
-# curl -H "Content-Type: application/json" -d '{"customer_id":1,"items":[{"id":2,"quantity":4},{"id":3,"quantity":6}]}' http://localhost:9090/api/add_new_order
+case $1 in
+    "run")
+        docker compose run -p 9090:9090 marketsvc
+        ;;
+    "customers")
+        curl http://localhost:9090/api/customers
+        ;;
+    "custorders")
+        curl http://localhost:9090/api/orders?cust_id=1
+        ;;
+    "ordertotal")
+        curl http://localhost:9090/api/order_total?order_id=1
+        ;;
+    "ordersbet")
+        curl "http://localhost:9090/api/orders_between_dates?after=2024-03-14&before=2024-03-22"
+        ;;
+    "neworder")
+        curl -H "Content-Type: application/json" -d '{"customer_id":1,"items":[{"id":2,"quantity":4},{"id":3,"quantity":6}]}' http://localhost:9090/api/add_new_order
+        ;;
+    *)
+        echo "unknown cmd: I know [run, customers, custorders, ordertotal, ordersbet, neworder, orderstotal]"
+esac
