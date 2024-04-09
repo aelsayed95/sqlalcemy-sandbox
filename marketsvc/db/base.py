cbc7764b297de8808e4afe352ef6700faf2bf9a7
Aya Elsayed <aelsayed12@bloomberg.net>
2024-04-09 18:43:57 +0000

    diffing

--- marketsvc/db/base.py
+++ marketsvc/db/base.py
@@ -9,19 +9,14 @@ from sqlalchemy.ext.asyncio import (
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.pool import NullPool
 
-DB_USER = os.environ.get("POSTGRES_USER")
-DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-DB_PORT = os.environ.get("POSTGRES_PORT")
-DB_NAME = os.environ.get("POSTGRES_DB")
-DB_HOST = "marketdb"
 
 url_object = URL.create(
     "postgresql+asyncpg",
-    username=DB_USER,
-    password=DB_PASSWORD,
-    host=DB_HOST,
-    database=DB_NAME,
-    port=DB_PORT,
+    username=os.environ.get("POSTGRES_USER"),
+    password=os.environ.get("POSTGRES_PASSWORD"),
+    host=os.environ.get("POSTGRES_DB"),
+    database=os.environ.get("POSTGRES_DB"),
+    port=os.environ.get("POSTGRES_PORT"),
 )
 
 engine = create_async_engine(url_object, poolclass=NullPool, echo=True)
